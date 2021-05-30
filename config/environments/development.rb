b3fc29bad965bc3d9c0918b64f0dfc4a2b319961
KUNIMOTO-M <psmith.mk0203@gmail.com>
2021-05-30 11:34:15 +0900

    本番環境での設定を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,12 +13,8 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.log_level = :debug
-  if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
+  
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
