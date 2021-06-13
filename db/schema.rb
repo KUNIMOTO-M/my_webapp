c3fba0828ddad6d9ba101034330c1092e96e4ba2
KUNIMOTO-M <psmith.mk0203@gmail.com>
2021-06-14 07:15:48 +0900

    バージョンをVue導入以前に変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_070037) do
+ActiveRecord::Schema.define(version: 2021_05_25_144831) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -119,7 +119,6 @@ ActiveRecord::Schema.define(version: 2021_06_04_070037) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "background_image"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
