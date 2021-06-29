2d2662d0e3b03f156dc869d1dffb1a051527468b
KUNIMOTO-M <psmith.mk0203@gmail.com>
2021-06-29 14:34:49 +0900

    s3を実装

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
   # リサイズしたり画像形式を変更するのに必要
   include CarrierWave::RMagick
 
