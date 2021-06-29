e4efa36a20bd092861667825157adda5169daff2
KUNIMOTO-M <psmith.mk0203@gmail.com>
2021-06-30 03:30:14 +0900

    本番環境に対応

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,4 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
   # リサイズしたり画像形式を変更するのに必要
   include CarrierWave::RMagick
 
